579bfacca97ea019aaffa0d71c2a74efb066b1ee
jvlivsc <to.julius80@gmail.com>
2021-08-18 11:44:21 +0300

    fixes

--- sh/update.sh
+++ sh/update.sh
@@ -1,28 +1,33 @@
 #! /bin/bash
 
 HOME_DIR="/opt/kat_client/"
-echo Home dir is $HOME_DIR
 SSH_KEY="../id_tunnel_rsa"
 SCRIPT="/home/kat/install/kat_client/"
 SERVER="kat@10.2.3.55"
+
 main(){
-    LAST_VERSION=$(cat "../VERSION")
-    [ "$LAST_VERSION" == "" ] && LAST_VERSION=0
-    CHECK_VERSION=$(ssh -i $SSH_KEY -o StrictHostKeyChecking=no ${SERVER} "cat install/kat_client/VERSION")
+    LAST_VERSION=$(head -n 1 "../VERSION")
+    LAST_VERSION=${LAST_VERSION##*.}
+    [ -z $LAST_VERSION ] && LAST_VERSION=0
+    echo "Last version: $LAST_VERSION"
 
-    [ "$CHECK_VERSION" == "" ] && CHECK_VERSION=0
-
-    if [ "$CHECK_VERSION" == "$LAST_VERSION" ]
+    CHECK_VERSION=$(ssh -i $SSH_KEY -o StrictHostKeyChecking=no ${SERVER} "head -n 1 install/kat_client/VERSION")
+    CHECK_VERSION=${CHECK_VERSION##*.}
+    [ -z $CHECK_VERSION ] && CHECK_VERSION=0
+    echo "New version: $CHECK_VERSION"
+    if [ $CHECK_VERSION -le $LAST_VERSION ]
     then
-        echo "No need to update $LAST_VERSION to $CHECK_VERSION"
+        echo "No need to update"
     else
-        echo "Need to update $LAST_VERSION to $CHECK_VERSION"
+        echo "Updating $LAST_VERSION to $CHECK_VERSION"
         rsync -a -e "ssh -i $SSH_KEY -o StrictHostKeyChecking=no" ${SERVER}:${SCRIPT} ${HOME_DIR}
-        [ "$?" -eq "0" ] || echo "Can not download update"
+        [ "$?" -eq "0" ] || echo "Can not download update"; return $?
         # chmod 740 ${HOME_DIR}
-        # systemctl daemon-reload
-        # systemctl restart kat-remote.service
+        systemctl daemon-reload
+        systemctl restart kat-client.service
+        echo "Done!"
     fi
+    return 0
 }
 
 main
