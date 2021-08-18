b380797c77529971d57e2b41eb33da9557ce0e76
jvlivsc <to.julius80@gmail.com>
2021-08-18 12:02:28 +0300

    fux

--- sh/update.sh
+++ sh/update.sh
@@ -6,22 +6,23 @@ SCRIPT="/home/kat/install/kat_client/"
 SERVER="kat@10.2.3.55"
 
 main(){
-    LAST_VERSION=$(head -n 1 "../VERSION")
-    LAST_VERSION=${LAST_VERSION##*.}
+    LAST_VERSION=$(head -n 1 "../VERSION" | cut -d '.' -f2)
+    # LAST_VERSION=${LAST_VERSION##*.}
     [ -z $LAST_VERSION ] && LAST_VERSION=0
     echo "Last version: $LAST_VERSION"
 
-    CHECK_VERSION=$(ssh -i $SSH_KEY -o StrictHostKeyChecking=no ${SERVER} "head -n 1 install/kat_client/VERSION")
-    CHECK_VERSION=${CHECK_VERSION##*.}
+    CHECK_VERSION=$(ssh -i $SSH_KEY -o StrictHostKeyChecking=no ${SERVER} "head -n 1 install/kat_client/VERSION | cut -d '.' -f2")
+    CHECK_VERSION=${CHECK_VERSION//[$'\t\r\n']}
     [ -z $CHECK_VERSION ] && CHECK_VERSION=0
     echo "New version: $CHECK_VERSION"
+
     if [ $CHECK_VERSION -le $LAST_VERSION ]
     then
         echo "No need to update"
     else
         echo "Updating $LAST_VERSION to $CHECK_VERSION"
         rsync -a -e "ssh -i $SSH_KEY -o StrictHostKeyChecking=no" ${SERVER}:${SCRIPT} ${HOME_DIR}
-        [ "$?" -eq "0" ] || echo "Can not download update"; return $?
+        [ $? -eq 0 ] || echo "Can not download update"; return $?
         # chmod 740 ${HOME_DIR}
         systemctl daemon-reload
         systemctl restart kat-client.service
